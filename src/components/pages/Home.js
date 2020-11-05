019ea436138548a3400d9d9ad21e910416d2bf92
Ben Berger <59838678+benjberg@users.noreply.github.com>
2020-11-05 09:56:56 -0500

    init

---
+++ src/components/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../../App.css'
+import Hero from '../Hero.js'
+import Cards from '../Cards.js'
+
+function Home() {
+    return(
+        <> 
+        <Hero/>
+        <Cards/>
+        </>
+    )
+}
+
+export default Home;
